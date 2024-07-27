1e36fa1943009edb497a6166349418d1aafde05c
yonat <Yonatan.rahamim1234@gmail.com>
2024-07-27 19:38:44 +0300

    Start

---
+++ main.py
@@ -0,0 +1,10 @@
+# Press the green button in the gutter to run the script.
+import ErrorCorrection
+
+if __name__ == '__main__':
+    textlist=[["MAKEINTEGER","8","NUMY"]
+    ErrorCorrection.tosimple()
+
+
+
+    ]
